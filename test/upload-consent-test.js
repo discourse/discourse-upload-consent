b5956d895bf29a33d207238a7ca703517cd8933e
David Taylor <david@taylorhq.com>
2024-03-27 16:49:27 +0000

    DEV: Update linting

--- test/upload-consent-test.js
+++ test/upload-consent-test.js
@@ -1,6 +1,6 @@
-import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import { click, fillIn, visit } from "@ember/test-helpers";
 import { test } from "qunit";
+import { acceptance } from "discourse/tests/helpers/qunit-helpers";
 import I18n from "I18n";
 
 acceptance("Upload consent - On non-specified categories", function (needs) {
